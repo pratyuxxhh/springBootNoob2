1f3c944d045d83b29f703645cd9e1b8d38f56102
pratyuxxhh <ishu.kool2406@gmail.com>
2024-12-17 20:58:14 +0530

    project initialized

---
+++ springBootNoob2/src/main/java/com/example/springBootNoob2/SpringBootNoob2Application.java
@@ -0,0 +1,13 @@
+package com.example.springBootNoob2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNoob2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootNoob2Application.class, args);
+	}
+
+}
