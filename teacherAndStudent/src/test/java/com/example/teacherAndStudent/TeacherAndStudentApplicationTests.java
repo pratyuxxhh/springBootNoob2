9605f21bfbe99bd6e0d49babe88b68a667f2d183
pratyuxxhh <ishu.kool2406@gmail.com>
2024-12-17 21:28:34 +0530

    project initiated

---
+++ teacherAndStudent/src/test/java/com/example/teacherAndStudent/TeacherAndStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teacherAndStudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherAndStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
